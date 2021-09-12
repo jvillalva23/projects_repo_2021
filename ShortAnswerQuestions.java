22d8f6fffa11183eca6c204684fa7d9e294d4113
Jesus <Jvillalba23@yahoo.com>
2021-09-12 15:59:13 -0500

    questionaire

---
+++ ShortAnswerQuestions.java
@@ -0,0 +1,51 @@
+public class ShortAnswerQuestions {
+    /*What is source code?
+    a source code is a set of instructions and commands.
+
+    What makes computers so “smart”, anyway?
+    a computer is smart because it can do a lot of calculations per second.
+
+    What is the purpose of a programming language?
+    To set instructions to a computer
+
+    What are 3 examples of programming languages? Why are there so many of these?
+    Java, Python, C++. To set different instructions for different users and computers. Like cars, different types to ease the user, but they all do the same thing but some do them better than others.
+
+    What is a variable? Why are variables useful?
+    a variable is useful so it can transmits object data throughout the program
+
+    What is the difference between a single-equals (=) and a double-equals (==)?
+    the single equal signs means you are giving a new meaning to the variable, the two equal signs means you are comparing the variables
+
+    What is the difference between a double-equals (==) and a triple-equals (===)?
+    two equal signs is an option, three equal signs means it should be the same if not it will crash
+
+    Why does the developer console exist?
+    provides you with the ability to write, manage, and monitor programs.
+
+    When we talk about “conditional” statements, what does that mean? What is an example?
+    Conditional sentences are sentences that express one thing contingent on something else, e.g. "If it rains, the picnic will be cancelled". They are so called because the impact of the main clause of the sentence is conditional on the dependent clause.
+    
+    Why would we want FOR or WHILE loops in our source code?
+    A "For" Loop is used to  to repeat a bit of code over and over, or repeat it but change something about it each time. 
+    A "While" Loop is used to repeat a specific block of code an unknown number of times, until a condition is me
+
+    When would you use a WHILE loop, instead of a FOR loop?
+    when you want to get data without having the chance to change the data
+
+    What is a function? Why would we use functions?
+    Functions are a good alternative to having repeating blocks of code in a program. Functions also increase the reusability of code. Values can be passed to a function using variables – we call these parameters or arguments. Functions can also return values.
+
+    How many values can you receive back from a function? How many values can you send in?
+    one
+
+    What is an array? How many values does it hold?
+    arrays are list of variables, and it will hold up to 100 variables.
+
+    What is a T-diagram and why should I know how to use one?
+    T-diagrams consist of a set of “puzzle pieces” representing compilers and other related language processing programs. ... They are most commonly found describing complicated processes for bootstrapping, porting, and self-compiling of compilers, interpreters, and macro-processors.
+
+    What are the two ways to comment JS code? When would you use one versus the other?
+    There are two types of comments used in JavaScript: single-line comments and block comments. Block comments are written with opening and closing tags and allow you to write comments that span across multiple lines. The opening and closing tags for block comments are: “/*” for opening a comment
+    */
+}

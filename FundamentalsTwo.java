c8ab1160be119b5d4b893fda408dcafcb22dee60
Jesus <Jvillalba23@yahoo.com>
2021-09-11 23:34:00 -0500

    fundamentals two

---
+++ FundamentalsTwo.java
@@ -0,0 +1,79 @@
+public class FundamentalsTwo {
+    public int countDown(int i){
+        number = 100;
+        for (i = number; i >= 0; i–-) {
+                countDown = new countDown.push(i);
+        }
+        return number;    
+    }
+
+    public int printAndReturn(int a, int b){
+        System.out.println(a);
+        return b;
+    }
+
+    public int[] firstPlusLength(int[] number){
+        if (int[0] != number) {
+                int[0] = 0 + number.length;
+            } else {
+                    int[0] = int[0] + number.length;
+                }
+            System.out.println(number);
+        }
+        return number;
+    }
+
+    public int[] valueGreaterSecond(int[] number){
+        number = [1,3,5,7,9,131];
+        for (i = 0; i > number[1], i++) {
+            System.out.println(number[i]);
+        }
+        return number;
+    }
+
+    public int[] greaterThanSecond(int[] arr){
+        int[] arr = [1,2,3,4,5,6];
+        if (arr.length == 0) {
+            System.out.println("The array is empty");
+        } else if (arr.length == 1) {
+            System.out.println("There is only 1 element");
+        } else {
+            for (var i = 0; i arr[1]) {
+                arr.push(arr[i]);
+            }  
+        }
+        return arr;
+    }
+
+    public int[] thisLength(int[] num1, int[] num2){
+        int[] newArr = [2, 2];
+        newArr.push(num1, num2);
+        if (num1 == num2) {
+            System.out.println("Jinx!");
+        }
+        return newArr;
+    }
+    
+    public int[] firstValues(int[] arr){
+        arr = [10,1,2,3];
+        if (arr[0] > arr.length) {
+            System.out.println("Too big!");
+        } else if (arr[0] < arr.length) {
+            System.out.println("Tool!");
+        } else {
+            System.out.println("Just!");
+        }
+    }
+
+    public int fahrenheitToCelcius(int celsius){
+        celsius = 100;
+        celsius = Math.floor((celsius – 32) * 5/9);
+        return celsius;
+    }
+
+    public int celciusToFahrenheit(int fahrenheit){
+        fahrenheit = 50;
+        fahrenheit = Math.floor((fahrenheit * 9/5) + 32);
+        return fahrenheit;
+    }
+}
